796873e99a1340b92fc1b11e22da9ca77ed42174
Haile-08 <haile4cmd@gmail.com>
2022-12-15 05:50:43 -0800

    Use a new method to hold the temp value

--- swap.c
+++ swap.c
@@ -12,21 +12,21 @@
 void swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
-	int num1, num2;
+	int num, len = 0;
 
 	temp = *stack;
-	num1 = temp->n;
-	*stack = (*stack)->next;
-	free(temp);
-	temp = *stack;
-	num2 = temp->n;
-	*stack = (*stack)->next;
-	free(temp);
-	if (*stack == NULL)
+	while (temp)
+	{
+		temp = temp->next;
+		len++;
+	}
+	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short", line_number);
 		exit(EXIT_FAILURE);
 	}
-	newnode(stack, num1);
-	newnode(stack, num2);
+	temp = *stack;
+	num = temp->n;
+	temp->n = temp->next->n;
+	temp->next->n = num;
 }
