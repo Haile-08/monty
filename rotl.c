475c2084cfe66a90d37e66517815f925f7660275
Haile-08 <haile4cmd@gmail.com>
2022-12-16 01:39:16 -0800

    Add Documentaion

--- rotl.c
+++ rotl.c
@@ -14,6 +14,8 @@ void rotl(stack_t **stack, unsigned int line_number)
 	stack_t *temp;
 	int top;
 
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
 	(void)line_number;
 	temp = *stack;
 	top = temp->n;
---
+++ rotr.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * rotr - rotr
+ * @stack: the top of the stack
+ * @line_number: the number of lines we read
+ *
+ * Description: rotates the stack to the bottom
+ * Return: none
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+
